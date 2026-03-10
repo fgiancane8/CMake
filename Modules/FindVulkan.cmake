1dd846b0d88e3c54985bedc11a805df5fbab205e
Giancane, Francesco <fgiancan@qti.qualcomm.com>
2026-03-10 14:35:14 +0000

    FindVulkan: Search for VkLayer libraries on iOS
    On non-iOS systems, Vulkan Layers are loaded dynamically by the Vulkan
    Loader at run-time.  On iOS systems, they are provided as frameworks to
    be bundled with an application, so there must be a way to discover those
    dependencies as well.
    
    Inspired by the FindVulkan module in `KhronosGroup/Vulkan-Samples`.
    
    Co-Authored-By: Brad King <brad.king@kitware.com>

--- Modules/FindVulkan.cmake
+++ Modules/FindVulkan.cmake
@@ -222,6 +222,96 @@ This module defines the following variables:
   Boolean indicating whether the SDK provides the DirectX shader compiler CLI
   tool.
 
+``Vulkan_Layer_API_DUMP``
+  .. versionadded:: 4.4
+
+  String indicating the `API_DUMP` Vulkan Layer library path.
+
+``Vulkan_Layer_API_DUMP_FOUND``
+  .. versionadded:: 4.4
+
+  Boolean indicating whether the `API_DUMP` Vulkan Layer library was found.
+
+``Vulkan_Layer_CRASH_DIAGNOSTIC``
+  .. versionadded:: 4.4
+
+  String indicating the `CRASH_DIAGNOSTIC` Vulkan Layer library path.
+
+``Vulkan_Layer_CRASH_DIAGNOSTIC_FOUND``
+  .. versionadded:: 4.4
+
+  Boolean indicating whether the `CRASH_DIAGNOSTIC` Vulkan Layer library was found.
+
+``Vulkan_Layer_GFXRECONSTRUCT``
+  .. versionadded:: 4.4
+
+  String indicating the `GFXRECONSTRUCT` Vulkan Layer library path.
+
+``Vulkan_Layer_GFXRECONSTRUCT_FOUND``
+  .. versionadded:: 4.4
+
+  Boolean indicating whether the `GFXRECONSTRUCT` Vulkan Layer library was found.
+
+``Vulkan_Layer_KHRONOS_PROFILES``
+  .. versionadded:: 4.4
+
+  String indicating the `KHRONOS_PROFILES` Vulkan Layer library path.
+
+``Vulkan_Layer_KHRONOS_PROFILES_FOUND``
+  .. versionadded:: 4.4
+
+  Boolean indicating whether the `KHRONOS_PROFILES` Vulkan Layer library was found.
+
+``Vulkan_Layer_SHADER_OBJECT``
+  .. versionadded:: 4.4
+
+  String indicating the `SHADER_OBJECT` Vulkan Layer library path.
+
+``Vulkan_Layer_SHADER_OBJECT_FOUND``
+  .. versionadded:: 4.4
+
+  Boolean indicating whether the `SHADER_OBJECT` Vulkan Layer library was found.
+
+``Vulkan_Layer_SYNC2``
+  .. versionadded:: 4.4
+
+  String indicating the `SYNCHRONIZATION2` Vulkan Layer library path.
+
+``Vulkan_Layer_SYNC2_FOUND``
+  .. versionadded:: 4.4
+
+  Boolean indicating whether the `SYNCHRONIZATION2` Vulkan Layer library was found.
+
+``Vulkan_Layer_VALIDATION``
+  .. versionadded:: 4.4
+
+  String indicating the `VALIDATION` Vulkan Layer library path.
+
+``Vulkan_Layer_VALIDATION_FOUND``
+  .. versionadded:: 4.4
+
+  Boolean indicating whether the `VALIDATION` Vulkan Layer library was found.
+
+``Vulkan_Layer_MONITOR``
+  .. versionadded:: 4.4
+
+  String indicating the `MONITOR` Vulkan Layer library path.
+
+``Vulkan_Layer_MONITOR_FOUND``
+  .. versionadded:: 4.4
+
+  Boolean indicating whether the `MONITOR` Vulkan Layer library was found.
+
+``Vulkan_Layer_SCREENSHOT``
+  .. versionadded:: 4.4
+
+  String indicating the `SCREENSHOT` Vulkan Layer library path.
+
+``Vulkan_Layer_SCREENSHOT_FOUND``
+  .. versionadded:: 4.4
+
+  Boolean indicating whether the `SCREENSHOT` Vulkan Layer library was found.
+
 Cache Variables
 ^^^^^^^^^^^^^^^
 
@@ -277,6 +367,51 @@ The following cache variables may also be set:
   .. versionadded:: 3.25
 
   The path to the DirectX shader compiler CLI tool.
+
+``Vulkan_Layer_API_DUMP``
+  .. versionadded:: 4.4
+
+  The path to the `API_DUMP` Vulkan Layer library.
+
+``Vulkan_Layer_CRASH_DIAGNOSTIC``
+  .. versionadded:: 4.4
+
+  The path to the `CRASH_DIAGNOSTIC` Vulkan Layer library.
+
+``Vulkan_Layer_GFXRECONSTRUCT``
+  .. versionadded:: 4.4
+
+  The path to the `GFXRECONSTRUCT` Vulkan Layer library.
+
+``Vulkan_Layer_KHRONOS_PROFILES``
+  .. versionadded:: 4.4
+
+  The path to the `KHRONOS_PROFILES` Vulkan Layer library.
+
+``Vulkan_Layer_SHADER_OBJECT``
+  .. versionadded:: 4.4
+
+  The path to the `SHADER_OBJECT` Vulkan Layer library.
+
+``Vulkan_Layer_SYNC2``
+  .. versionadded:: 4.4
+
+  The path to the `SYNCHRONIZATION2` Vulkan Layer library.
+
+``Vulkan_Layer_VALIDATION``
+  .. versionadded:: 4.4
+
+  The path to the `VALIDATION` Vulkan Layer library.
+
+``Vulkan_Layer_MONITOR``
+  .. versionadded:: 4.4
+
+  The path to the `MONITOR` Vulkan Layer library.
+
+``Vulkan_Layer_SCREENSHOT``
+  .. versionadded:: 4.4
+
+  The path to the `SCREENSHOT` Vulkan Layer library.
 
 Hints
 ^^^^^
@@ -420,6 +555,36 @@ find_library(Vulkan_LIBRARY
   )
 mark_as_advanced(Vulkan_LIBRARY)
 
+# On iOS search for Validation Layers, which must be explicitly linked.
+if (IOS)
+  foreach(layer IN ITEMS
+      api_dump
+      crash_diagnostic
+      gfxreconstruct
+      khronos_profiles
+      khronos_shader_object
+      khronos_synchronization2
+      khronos_validation
+      monitor
+      screenshot
+    )
+    string(TOUPPER "${layer}" LAYER)
+    find_library(Vulkan_Layer_${LAYER}
+      NAMES VkLayer_${layer}
+      HINTS
+      ${_Vulkan_hint_library_search_paths}
+      OPTIONAL
+    )
+    mark_as_advanced(Vulkan_Layer_${LAYER})
+    if(Vulkan_Layer_${LAYER})
+      set(Vulkan_Layer_${LAYER}_FOUND TRUE)
+    else()
+      set(Vulkan_Layer_${LAYER}_FOUND FALSE)
+    endif()
+  endforeach()
+endif()
+
+# Search for requested components.
 if(glslc IN_LIST Vulkan_FIND_COMPONENTS)
   find_program(Vulkan_GLSLC_EXECUTABLE
     NAMES glslc
